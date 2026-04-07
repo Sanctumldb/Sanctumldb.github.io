c49c76902be0fc082122c66485fb1a8296c9cb2b
Sanctumldb <73103779+Sanctumldb@users.noreply.github.com>
2026-04-07 06:17:18 +0000

    Deploying to gh-pages from @ Sanctumldb/Sanctumldb.github.io@5ae41a454059ea35a15de0480c5444b8bc19f883 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-services",
-          title: "services",
+          title: "Services",
           description: "",
           section: "Navigation",
           handler: () => {
